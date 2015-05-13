96316009eb3fb1b36f7d21ef19185477736279cd
Gabriela de Souza <gabsouza98@gmail.com>
2015-05-13 17:34:01 -0300

    Exercícios 1.1 (Não esta pronto)

---
+++ src/modulo-04-java-avançado/Filmator/src/Filmator/Ator.java
@@ -0,0 +1,29 @@
+package Filmator;
+
+
+public class Ator {
+
+	private String nome;
+	private int idade;
+	
+	public String toString(){
+		return "Nome: " + this.nome + " Idade: " + this.idade;
+	}
+	
+	public  Ator (String nome){
+		this.nome = nome;
+	}
+	
+	public Ator (String nome, int idade){
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public int getIdade(){
+		return this.idade;
+	}
+}
--- src/modulo-04-java-avançado/Listator/src/Listator/Listator.java
+++ src/modulo-04-java-avançado/Listator/src/Listator/Listator.java
@@ -1,5 +1,32 @@
 package Listator;
 
+import java.io.File; 
+
 public class Listator {
 
+	public static void main(String[] args) {
+		  validaParametros(args);
+		  String diretorio = args[0];
+
+		  String[] files = buscaArquivosDiretorio(diretorio);
+		    
+		  imprimeArquivos(files);
+
+	}
+		
+	private static void validaParametros(String[] args) {
+	    if(args.length == 0){
+	        throw new IllegalStateException("Nenhum diretório informado");
+	    }
+	}
+	
+	private static void imprimeArquivos(String[] files) {
+	    for(String file : files){
+	        System.out.println(file);
+	    }
+	}
+	
+	public static String[] buscaArquivosDiretorio(String diretorio) {
+	    return new File(diretorio).list();
+	}
 }
