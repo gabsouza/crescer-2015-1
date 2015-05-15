d1d69c3acd5bfd906854958eb1d44a420d0fdb3d
Gabriela de Souza <gabsouza98@gmail.com>
2015-05-15 14:10:54 -0300

    Exercício 2.1 pronto

--- src/modulo-04-java-avançado/Filmator/Test/Test/TesteAtor.java
+++ src/modulo-04-java-avançado/Filmator/Test/Test/TesteAtor.java
@@ -10,14 +10,35 @@ public class TesteAtor {
 	
 	@Test
 	public void TestToString(){
+		 String nomeEsperado = "Fernando";
+		 int idadeEsperada = 16;
+		 	Ator ator = new Ator(nomeEsperado, idadeEsperada);
+		 	String mensagemEsperada = "Nome: Fernando Idade: 16";
+		 	assertEquals (mensagemEsperada, ator.toString());
+	        assertEquals(nomeEsperado, ator.getNome());
+	        assertEquals(idadeEsperada, ator.getIdade());
+		
+		
 		
 	}
 	
 	@Test
-	public void Ator(){
+	public void AtorRecebeNome(){
 		 String nomeEsperado = "Fernando";
 	        Ator nome = new Ator (nomeEsperado);
 	        assertEquals(nomeEsperado, nome.getNome());
 	}
+	
+	@Test
+	public void AtorRecebeNomeEIdade(){
+		 String nomeEsperado = "Fernando";
+		 int idadeEsperada = 16;
+		 	Ator ator = new Ator(nomeEsperado, idadeEsperada);
+	        assertEquals(nomeEsperado, ator.getNome());
+	        assertEquals(idadeEsperada, ator.getIdade());
+		
+	}
+	
+	
 
 }
