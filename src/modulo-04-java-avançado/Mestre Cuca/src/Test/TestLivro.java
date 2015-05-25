211d4caac33a1dde5a1570c00d485833bb27c31a
Gabriela de Souza <gabrieladessouza@gmail.com>
2015-05-25 14:35:24 -0300

    arrumando testes

--- src/modulo-04-java-avançado/Mestre Cuca/src/Test/TestLivro.java
+++ src/modulo-04-java-avançado/Mestre Cuca/src/Test/TestLivro.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import mestrecuca.Ingrediente;
-import mestrecuca.IngredienteReceita;
 import mestrecuca.Instrucao;
 import mestrecuca.Livro;
 import mestrecuca.Receita;
@@ -21,13 +20,10 @@ public class TestLivro {
 	public void testeInserirReceita() throws Exception {
 		Instrucao instrucao1 = new Instrucao("Misture tudo");
 		Instrucao instrucao2 = new Instrucao("Bata a massa");
-		Ingrediente ovo = new Ingrediente("Ovo", 0.30);
-		Ingrediente leite = new Ingrediente("leite", 2.00);
-		IngredienteReceita ingrediente1 = new IngredienteReceita(ovo, 6.00, 
-														UnidadeMedida.UNIDADE);
-		IngredienteReceita ingrediente2 = new IngredienteReceita(leite, 2.00, 
-														UnidadeMedida.XICARA);
-		List<IngredienteReceita> ingredientes = new ArrayList<>();
+
+		Ingrediente ingrediente1 = new Ingrediente("ovo", 6.00, UnidadeMedida.UNIDADE);
+		Ingrediente ingrediente2 = new Ingrediente("leite", 2.00, UnidadeMedida.XICARA);
+		List<Ingrediente> ingredientes = new ArrayList<>();
 		ingredientes.add(ingrediente1);
 		ingredientes.add(ingrediente2);
 		
@@ -48,12 +44,10 @@ public class TestLivro {
 	public void testeAtualizar() throws Exception{
 		Instrucao instrucao1 = new Instrucao("Misture tudo");
 		Instrucao instrucao2 = new Instrucao("Bata a massa");
-		Ingrediente ovo = new Ingrediente("Ovo", 0.30);
-		Ingrediente leite = new Ingrediente("leite", 2.00);
-		IngredienteReceita ingrediente1 = new IngredienteReceita(ovo, 6.00, UnidadeMedida.UNIDADE);
-		IngredienteReceita ingrediente2 = new IngredienteReceita(leite, 2.00, UnidadeMedida.XICARA);
+		Ingrediente ingrediente1 = new Ingrediente("ovo", 6.00, UnidadeMedida.UNIDADE);
+		Ingrediente ingrediente2 = new Ingrediente("leite", 2.00, UnidadeMedida.XICARA);
 		
-		List<IngredienteReceita> ingredientes = new ArrayList<>();
+		List<Ingrediente> ingredientes = new ArrayList<>();
 		ingredientes.add(ingrediente1);
 		ingredientes.add(ingrediente2);
 		
@@ -67,13 +61,10 @@ public class TestLivro {
 		
 		Instrucao instrucaonova1 = new Instrucao("Separe a clara da gema antes de misturar os ingredientes");
 		Instrucao instrucaonova2 = new Instrucao("Bata tudo em uma batedeira");
-		Ingrediente chocolate = new Ingrediente ("Chocolate", 5.00);
-		Ingrediente farinha = new Ingrediente ("Farinha", 2.00);
-		IngredienteReceita ingredienteChocolate = new IngredienteReceita (chocolate, 0.100, UnidadeMedida.GRAMA);
-		IngredienteReceita ingredienteFarinha = new IngredienteReceita (farinha, 0.400, UnidadeMedida.GRAMA);
+		Ingrediente ingredienteChocolate = new Ingrediente ("chocolate", 0.100, UnidadeMedida.GRAMA);
+		Ingrediente ingredienteFarinha = new Ingrediente ("farinha", 0.400, UnidadeMedida.GRAMA);
 		
-		
-		List<IngredienteReceita> ingredientesNovos = new ArrayList<>();
+		List<Ingrediente> ingredientesNovos = new ArrayList<>();
 		ingredientesNovos.add(ingredienteChocolate);
 		ingredientesNovos.add(ingredienteFarinha);
 		
